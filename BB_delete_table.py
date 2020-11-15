22376f71256b46aced33e5dfa491a78b9a239ea3
Chrysalida <54242060+Chrysalida@users.noreply.github.com>
2020-11-15 11:25:34 +0300

    Add files via upload

---
+++ BB_delete_table.py
@@ -0,0 +1,48 @@
+"""THIS IS GENERAL VERSION
+"""
+
+from config import dsn_hostname, dsn_uid, dsn_pwd, dsn_driver, dsn_database, dsn_port, dsn_protocol
+import ibm_db
+
+#To be inserted before every start!
+TABLENAME="BB_table"
+
+#DB credentials, stored in config file
+dsn = (
+    "DRIVER={0};"
+    "DATABASE={1};"
+    "HOSTNAME={2};"
+    "PORT={3};"
+    "PROTOCOL={4};"
+    "UID={5};"
+    "PWD={6};").format(dsn_driver, dsn_database, dsn_hostname, dsn_port, dsn_protocol, dsn_uid, dsn_pwd)
+
+try:
+    conn = ibm_db.connect(dsn, "", "")
+    print ("Connected to database: ", dsn_database, "as user: ", dsn_uid, "on host: ", dsn_hostname)
+
+    # РАБОТАЕТ
+##    DeleteQuery = "delete from "+TABLENAME+" WHERE id="+'591342003'
+##    print(DeleteQuery)
+##    DeleteStmt = ibm_db.exec_immediate(conn, DeleteQuery)
+
+    # Тоже работает
+    DeleteQuery = "delete from "+TABLENAME
+    print(DeleteQuery)
+    DeleteStmt = ibm_db.exec_immediate(conn, DeleteQuery)
+
+    selectQuery = "select * from "+TABLENAME
+    selectStmt = ibm_db.exec_immediate(conn, selectQuery)
+
+    #Fetch the Dictionary for the first row
+    #ibm_db.fetch_both(selectStmt)
+
+    #Fetch the rest of the rows and print the ID and FNAME for those rows
+    while ibm_db.fetch_row(selectStmt) != False:
+        print (" ID:",  ibm_db.result(selectStmt, 0), " @username:",  ibm_db.result(selectStmt, "USERNAME"))
+
+except:
+    print ("something gone wrong, but I don't know what exactly ", ibm_db.conn_errormsg() )
+
+ibm_db.close(conn)
+print ("Connection closed")
